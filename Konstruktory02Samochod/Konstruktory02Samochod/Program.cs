efcd7b17b2294818a56793e4bd707b7ecce43938
aguua <48204399+aguua@users.noreply.github.com>
2019-10-18 17:34:42 +0200

    Update Program.cs

--- Konstruktory02Samochod/Konstruktory02Samochod/Program.cs
+++ Konstruktory02Samochod/Konstruktory02Samochod/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Konstruktory02Samochod
+namespace Autka
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Polozenie pol1 = new Polozenie(3,4);
+            Silnik s20 = new Silnik(2.0);
+            Silnik s10 = new Silnik(1.0);
+
+            Samochod s1 = new Samochod(1, "Focus", "Ford", 1.4, 10.0, 20.2);
+            Samochod s2 = new Samochod(2, "Astra", "Opel", s20, pol1);
+            Samochod s3 = new Samochod(3, "Punto", "Fiat", s10);
+            Samochod s4 = new Samochod(4, "C4", "Citroen");
+            Samochod s5 = new Samochod(12);
+            Samochod s6 = new Samochod();
+
+
+
+            Console.WriteLine(s1.PobierzInformacje());
+            Console.WriteLine(s2.PobierzInformacje());
+            Console.WriteLine(s3.PobierzInformacje());
+            Console.WriteLine(s4.PobierzInformacje());
+            Console.WriteLine(s5.PobierzInformacje());
+            Console.WriteLine(s6.PobierzInformacje());
+            Console.ReadKey();
+
+
         }
     }
 }
